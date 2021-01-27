b94f5e4e4232d3105b8ab10f0bc8a10505561800
KM Shibly Reza <kmshiblyreza1994@gmail.com>
2021-01-28 02:31:16 +0600

    Automatic Movement

---
+++ Assets/LeftbranchMovementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftbranchMovementController : MonoBehaviour
+{
+    Animator animator;
+    
+    int isWavingHash;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        //Increasing Preformance
+       
+        isWavingHash = Animator.StringToHash("isWaving");
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        bool isWaving = animator.GetBool(isWavingHash);
+        
+        bool waving = Input.GetKey("1");
+       
+
+
+        // When Button 1 Is Pressed.
+        if (!isWaving && waving)
+        {
+            // Setting isWaving1 Boolean To True.
+            animator.SetBool(isWavingHash, true);
+
+        }
+        // When Button 1 Isn't Pressed.
+        if (isWaving && !waving)
+        {
+            // Setting isWaving1 Boolean To False.
+            animator.SetBool(isWavingHash, false);
+
+        }
+
+
+    }
+}
+
